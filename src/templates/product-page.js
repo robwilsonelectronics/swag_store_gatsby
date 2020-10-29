877576901ac9777809677591287adbcdb56e2d87
Jason Lengstorf <jason@lengstorf.com>
2020-10-29 10:01:00 -0700

    feat: add a stock alert opt-in

--- src/templates/product-page.js
+++ src/templates/product-page.js
@@ -1,18 +1,20 @@
-
-import React, { Fragment } from "react";
-import { graphql, Link } from "gatsby";
-import Layout from "../components/layout";
-import ProductTypeLabel from "../components/product-type-label";
-import Cart from "../assets/cart.svg";
-import { useCart } from "../context/cart-context";
-import styles from "../styles/product-details.module.css";
-import SEO from "../components/seo";
-import { SizingChartShirts, SizingChartJammies } from "../components/sizing-charts";
-import { PasswordLock } from "../components/password-lock";
+import React, { Fragment } from 'react';
+import { graphql, Link } from 'gatsby';
+import Layout from '../components/layout';
+import ProductTypeLabel from '../components/product-type-label';
+import Cart from '../assets/cart.svg';
+import { useCart } from '../context/cart-context';
+import styles from '../styles/product-details.module.css';
+import SEO from '../components/seo';
+import {
+  SizingChartShirts,
+  SizingChartJammies,
+} from '../components/sizing-charts';
+import { PasswordLock } from '../components/password-lock';
 import ProductPageThumbnail from '../components/product-page-thumbnail.js';
 
-import SelectArrow from "../components/select-arrow";
-import { useAccess } from "../context/access-context";
+import SelectArrow from '../components/select-arrow';
+import { useAccess } from '../context/access-context';
 
 export const query = graphql`
   query($productID: String) {
@@ -20,6 +22,7 @@ export const query = graphql`
       handle
     }
     shopifyProduct(id: { eq: $productID }) {
+      id
       title
       description
       images {
@@ -49,9 +52,9 @@ export const query = graphql`
   }
 `;
 
-const formatPrice = (amount, currency = "USD") => {
-  return new Intl.NumberFormat("en-US", {
-    style: "currency",
+const formatPrice = (amount, currency = 'USD') => {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
     currency: currency,
   }).format(amount);
 };
@@ -62,7 +65,7 @@ const Product = ({ product }) => {
 
   const firstVariant = product.variants[0];
   const availableForSale = product.variants.find(
-    (variant) => variant.availableForSale
+    (variant) => variant.availableForSale,
   );
   const currency = firstVariant.priceV2.currencyCode;
   const price = formatPrice(firstVariant.priceV2.amount);
@@ -71,31 +74,67 @@ const Product = ({ product }) => {
     : null;
 
   const metafields = product.metafields.filter((field) => {
-    return field.key !== "Sizing Chart";
+    return field.key !== 'Sizing Chart';
   });
 
   const sizingChart = product.metafields.filter((field) => {
-    return field.key === "Sizing Chart";
+    return field.key === 'Sizing Chart';
   });
 
   const getSizingChart = (product) => {
-    if (product === "shirt") {
+    if (product === 'shirt') {
       return <SizingChartShirts />;
     }
-    if (product === "jammies") {
+    if (product === 'jammies') {
       return <SizingChartJammies />;
     }
   };
-  
+
   const handleSubmit = (event) => {
     event.preventDefault();
 
     const data = new FormData(event.target);
 
     addItemToCart({
-      variantId: data.get("variantId"),
-      quantity: data.get("quantity"),
+      variantId: data.get('variantId'),
+      quantity: data.get('quantity'),
     });
+  };
+
+  const handleStockSubmit = async (event) => {
+    event.preventDefault();
+
+    const data = new FormData(event.target);
+
+    const email = data.get('email');
+    const hubspotformid = data.get('hubspotformid');
+    const hubspotutk = data.get('hubspotutk');
+    const swag_product = data.get('swag_product');
+    const swag_product_id = data.get('swag_product_id');
+
+    console.log({
+      email,
+      hubspotformid,
+      hubspotutk,
+      swag_product,
+      swag_product_id,
+    });
+
+    const response = await fetch(
+      'https://www.netlify.com/.netlify/functions/hubspot',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email,
+          hubspotformid,
+          hubspotutk,
+          swag_product,
+          swag_product_id,
+        }),
+      },
+    ).then((res) => res.json());
+
+    console.log(response);
   };
 
   return (
@@ -131,13 +170,13 @@ const Product = ({ product }) => {
               </dl>
             </details>
           ) : (
-            " "
+            ' '
           )}
 
           <form
             onSubmit={handleSubmit}
             className={`${styles.form} ${
-              needsSizing ? styles.needsSizing : ""
+              needsSizing ? styles.needsSizing : ''
             }`}
           >
             {needsSizing ? (
@@ -207,13 +246,46 @@ const Product = ({ product }) => {
           <ProductPageThumbnail images={product.images} />
         </div>
       </div>
+
+      <div className={styles.details}>
+        <h2 className={`${styles.heading} ${styles.stockHeading}`}>
+          Get notified about new and restocked items!
+        </h2>
+
+        <form method="POST" onSubmit={handleStockSubmit}>
+          <input
+            type="hidden"
+            name="hubspotformid"
+            value="cc1a422f-7b03-46ca-9653-9d346c98b8c0"
+          />
+          <input type="hidden" name="hubspotutk" value="" />
+          <input type="hidden" name="swag_product" value={product.title} />
+          <input type="hidden" name="swag_product_id" value={product.id} />
+          <div className={styles.formRow}>
+            <label htmlFor="email">Email Address</label>
+            <input
+              className={styles.email}
+              id="email"
+              name="email"
+              type="email"
+              required
+            />
+          </div>
+          <div className={`${styles.formRow} ${styles.stockSubmit}`}>
+            <button type="submit" className={styles.button}>
+              Sign Up
+            </button>
+          </div>
+        </form>
+      </div>
+
       {sizingChart.length > 0 ? (
         <div className={styles.details}>
           <h2 className={styles.heading}>Sizing chart</h2>
           {getSizingChart(sizingChart[0].value)}
         </div>
       ) : (
-        " "
+        ' '
       )}
     </Fragment>
   );
@@ -225,13 +297,13 @@ const ProductPage = ({ data }) => {
   const product = data.shopifyProduct;
 
   // TODO centralize which collections are exclusive
-  const isProtectedCollection = ["netlify-exclusive"].includes(collection);
+  const isProtectedCollection = ['netlify-exclusive'].includes(collection);
 
   return (
     <Layout>
       {!isProtectedCollection || access ? (
         <Fragment>
-          <SEO metadata={{ summary: "summary", ...product }} />
+          <SEO metadata={{ summary: 'summary', ...product }} />
           <Product product={product} />
         </Fragment>
       ) : (
